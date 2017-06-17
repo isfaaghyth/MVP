45d08eebbb917835fe4aa20bb4c3f5d021a94d11
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-06-18 02:30:15 +0800

    add employeeholder

--- app/src/main/java/isfaaghyth/app/mvp/adapter/holder/EmployeeHolder.java
+++ app/src/main/java/isfaaghyth/app/mvp/adapter/holder/EmployeeHolder.java
@@ -1,8 +1,29 @@
 package isfaaghyth.app.mvp.adapter.holder;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import isfaaghyth.app.mvp.R;
+
 /**
  * Created by isfaaghyth on 6/18/17.
  */
 
-public class EmployeeHolder {
+public class EmployeeHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.txt_title) private TextView txtTitle;
+    @BindView(R.id.txt_desc) private TextView txtDesc;
+
+    public EmployeeHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void setContent(String title, String desc) {
+        txtTitle.setText(title);
+        txtDesc.setText(desc);
+    }
 }
